c90ba0ab81287e8b942f9a26e27f36beed17f3c2
ambili <ambilits99@gmail.com>
2024-09-29 10:23:40 +0530

    added

---
+++ just.py
@@ -0,0 +1,13 @@
+def greet(n):
+    
+    while True:
+        if(n==1):
+            print("hi")
+        if(n==2):
+            print("hello")
+        if(n==3):
+            print("ola")
+        else:
+            system.exit(0)
+greet(n)
+n=int(input("enter the choice"))
