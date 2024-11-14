3da3941f7693c30a788511cf9dd32721fba46be5
ambili <ambilits99@gmail.com>
2024-11-14 12:35:53 +0530

    added new

---
+++ loki.py
@@ -0,0 +1,6 @@
+# Loki, the God of mischief can brainwash any living person by touching him/her with his Mind stone,
+#  and has decided to break the avengers (a warrior group) to face into each other,
+#   so that they can turn against each other and make Loki’s evil plans easier. 
+#   Now all the avengers have some amount of strength that is denoted in integers. 
+#   Loki wants to brainwash the least amount of people possible, because he is lazy. 
+#   But he wants his team of avengers to win the battle. What is the number of avengers Loki will get brainwashed.
